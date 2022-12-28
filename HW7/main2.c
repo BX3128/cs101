dd854cfec2a08a199db14156c7676c865757af99
BX3128 <binx1394@gmail.com>
2022-12-28 15:12:24 +0800

    HW7

--- HW7/main2.c
+++ HW7/main2.c
@@ -21,13 +21,20 @@
 #include <stdlib.h>      // atoi()
 #include <stdbool.h>     // bool
 
+// struct prototype
+/*struct BIG {
+    char value[41];
+    bool sign;
+    char point;
+};*/
+
 // function prototype
 void message(char* msg);
 int PI(void);
 int PI2(void);
-
-// struct prototype
-struct BIG;
+int PI3(void);
+//BIG add(BIG a, BIG b);
+void test(void);
 
 // main()
 int main(int argc, char* argv[]) {
@@ -44,6 +51,9 @@ int main(int argc, char* argv[]) {
             case 3:
                 PI3();   // BIG
                 break;
+            case 4:
+                test();
+                break;
             default:
                 message("Unsupported alogrium\n");
                 break;
@@ -54,12 +64,12 @@ int main(int argc, char* argv[]) {
     return 0;            // exit program with error code 0
 }
 
-// struCct for 21 decimal digits BIG number
-struct BIG {
-  char value[21];    // 0.00000 00000 00000 00000
+// struct for 41 decimal digits BIG number
+/*struct BIG {
+  char value[41];    // 0.00000 00000 00000 00000 00000 00000 00000 ....
   bool sign = false; // false = 0 = positive;  true = 1 = negative
   char point = 1;    // default position of floating point
-}
+};*/
 
 // Generate an ouput and print it to the screen.
 void message(char* msg) {
@@ -85,24 +95,40 @@ int PI() {
 }
 
 int PI2() {
-    printf("Calculate Pi using Gregory-Leibniz Series: alogrium #2\n");
-    double PI = -3.14159;
-    double er =  0.0000001;  // error
-    int i = 1;           // iteration
+    printf("Calculate Pi using Gregory-Leibniz Series: alogrium #2 (long long)\n");
+    //             123456789012345678
+    long long PI = 000000000000000000ll;
+    long long er =      1000000000000ll;  // error
+    //                  1234567890123
+    int i;                    // iteration
 
-    for (i = 1; i < 400000; i++) {
-        PI += (((i+1) % 2) * (-2) + 1) * 4.0 / (2 * i - 1);
-        if (PI < er)
-            if (PI > -er) {
-                printf("%d, %.10f\n", i, PI+3.14159);
-                break;
-            }
+    for (i = 1; i < 800000; i++) { //    123456789012345678
+        PI += (((i+1) % 2) * (-2) + 1) * 400000000000000000ll / (2 * i - 1);
+        printf("%d, %lld, %lld\n", i, PI, PI/er);
+        if (PI/er == 314159)
+            break;
     }
+    printf("%d, %lld, %lld\n", i, PI, PI/er);
+    printf("x = %.12f\n", (double) (((i+1)%2)*(-2)+1)*4/(2*i-1));
     return i;
 }
 
 int PI3() {
-    printf("Calculate Pi using Gregory-Leibniz Series: alogrium #3\n");
+    printf("Calculate Pi using Gregory-Leibniz Series: alogrium #3 (big number, 41 digits)\n");
 
     
+}
+/*
+BIG add(BIG a, BIG b) {
+    return a;
+}*/
+
+void test() {
+    long long a = 3141592653ll;
+    long long b =      10000ll;
+    printf("%lld, %lld, %lld\n", a, b, a/b);
+    long long c = 314159265358979323ll;
+    //            123445678901234567
+    printf("%lld\n", c);
+    return;
 }
